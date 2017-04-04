bb108fa3536b7b0fa7057e200a9700c5e07a2855
cfu <cfu@e1z3r1p19.42.us.org>
2017-04-03 19:39:54 -0700

    need to do fds

--- get_next_line.c
+++ get_next_line.c
@@ -21,8 +21,7 @@ int		get_next_line(const int fd, char **line)
 		return (-1);
 	if (!gnl->data)
 		gnl->data = (char *)ft_memalloc(1);
-	ft_bzero(gnl->buf, BUFF_SIZE + 1);
-	while (ft_strchr(gnl->buf, '\n') == 0)
+	while (ft_strchr(gnl->data, '\n') == 0)
 	{
 		if ((ret = read(fd, gnl->buf, BUFF_SIZE)) == -1)
 			return (-1);
@@ -30,32 +29,24 @@ int		get_next_line(const int fd, char **line)
 			break ;
 		gnl->buf[ret] = '\0';
 		gnl->data = ft_strjoin(gnl->data, gnl->buf);
-		ft_bzero(gnl->buf, BUFF_SIZE + 1);
 	}
-	if (ft_search_str(gnl->data, '\n') > 0)
+	if (ft_search_str(gnl->data, '\n') >= 0)
 	{
 		*line = ft_strndup(gnl->data, ft_search_str(gnl->data, '\n'));
-		gnl->found_it = ft_strchr(gnl->data, '\n') + 1;
+		gnl->found_it = ft_strsub(gnl->data, ft_search_str(gnl->data, '\n') +
+			   	1, ft_strlen(gnl->data) - ft_search_str(gnl->data, '\n') - 1);
+		free(gnl->data);
+		gnl->data = gnl->found_it;
+		return (1);
 	}
-	if (ft_search_str(gnl->data, '\n') == -1 && ft_search_str(gnl->data, '\0') > 0)
+	else
 	{
-		*line = ft_strndup(gnl->data, ft_search_str(gnl->data, '\0'));
-		gnl->found_it = ft_strchr(gnl->data, '\0');
+		*line = ft_strdup(gnl->data);
+		gnl->found_it = ft_strnew(0);
 	}
-	if (ft_search_str(gnl->data, '\n') == 0 || ft_search_str(gnl->data, '\0') == 0)
-	{
-		*line = ft_strdup("");
-		if (ft_search_str(gnl->data, '\n') == 0)
-		{
-			gnl->found_it++;
-			gnl->data = gnl->found_it;
-			return (1);
-		}
-		else
-			gnl->found_it = ft_strchr(gnl->data, '\0');
-	}
+	free(gnl->data);
 	gnl->data = gnl->found_it;
-	return ((!ret && !ft_strlen(gnl->buf) && !ft_strlen(*line)) ? 0 : 1);
+	return ((!ret && !ft_strlen(gnl->data) && !ft_strlen(*line)) ? 0 : 1);
 }
 
 /*int		main(int ac, char **av)
@@ -64,18 +55,13 @@ int		get_next_line(const int fd, char **line)
 	int			fd;
 	
 	line = NULL;
-	fd = open(av[1], O_RDONLY);
 	if (ac > 1)
 	{
-			printf("%i\n%s\n", get_next_line(fd, &line), line);
-			printf("%i\n%s\n", get_next_line(fd, &line), line);
-			printf("%i\n%s\n", get_next_line(fd, &line), line);
-			printf("%i\n%s\n", get_next_line(fd, &line), line);
-			printf("%i\n%s\n", get_next_line(fd, &line), line);
-			printf("%i\n%s\n", get_next_line(fd, &line), line);
-			printf("%i\n%s\n", get_next_line(fd, &line), line);
-			printf("%i\n%s\n", get_next_line(fd, &line), line);
+			fd = open(av[1], O_RDONLY);
+			int ret;
+			while ((ret = get_next_line(fd, &line)) > 0)
+				printf("%d: %s\n", ret, line);
+			printf("%d\n", ret);
 	}
 	return (0);
-}
-*/
+}*/
